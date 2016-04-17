3aed513855140e6367765c69aca30fc1d9f76245
George Lambert <georgethomaslambert@gmail.com>
2016-04-17 20:56:02 +0100

    update

--- settings/base.py
+++ settings/base.py
@@ -49,15 +49,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'blog.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
--- settings/local.py
+++ settings/local.py
@@ -1,3 +1,10 @@
 from base import *
 
 GOOGLE_ANALYTICS_TAG = 0
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
